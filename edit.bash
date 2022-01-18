e96a133ca5ae0efa477c9baf4b916c4cbd565e1f
Paul Tarvydas <guitarvydas@gmail.com>
2022-01-17 23:48:19 -0500

    emit enough for hand-edit ; create identity-s.glue

--- edit.bash
+++ edit.bash
@@ -2,4 +2,4 @@
 prep=../prep/prep
 ohmp=../ohm-parse
 #$prep '.' '$' $ohmp/ohm.ohm $ohmp/identity-ohm.glue --stop=1 <s.ohm
-$prep '.' '$' $ohmp/emitglue.ohm $ohmp/emitglue.glue --support=$ohmp/support.js --trace --stop=1 <s.ohm
+$prep '.' '$' $ohmp/emitglue.ohm $ohmp/emitglue.glue --support=$ohmp/support.js --stop=1 <s.ohm
